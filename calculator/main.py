0b872e41ee3a59ec25d4ef42c16d676c6d6234f3
Hollinss <hj84@njit.edu>
2021-11-09 18:30:16 -0500

    TASK: Updated main.py with new path

--- calculator/main.py
+++ calculator/main.py
@@ -1,9 +1,4 @@
 """ This is the increment function"""
-from calc_mod.calculations.addition import Addition
-from calc_mod.calculations.subtraction import Subtraction
-from calc_mod.calculations.multiplication import Multiplication
-from calc_mod.calculations.division import Division
-
 class Calculator:
     """ This is the Calculator class"""
     #history array to hold values
@@ -34,27 +29,3 @@ class Calculator:
     def get_object_of_latest_calculation_added():
         """Returns latest calculation class from history"""
         return Calculator.history[-1]
-    @staticmethod
-    def add_number(value_a, value_b):
-        """add number to result"""
-        addition = Addition.create(value_a, value_b)
-        Calculator.add_calculation_to_history(addition)
-        return Calculator.get_result_of_latest_calculation_added()
-    @staticmethod
-    def subtract_number(value_a, value_b):
-        """subtract numbers"""
-        subtraction = Subtraction.create(value_a, value_b)
-        Calculator.add_calculation_to_history(subtraction)
-        return Calculator.get_result_of_latest_calculation_added()
-    @staticmethod
-    def multiply_number(value_a, value_b):
-        """multiply two numbers"""
-        multiplication = Multiplication.create(value_a, value_b)
-        Calculator.add_calculation_to_history(multiplication)
-        return Calculator.get_result_of_latest_calculation_added()
-    @staticmethod
-    def divide_number(value_a,value_b):
-        """divide two numbers"""
-        division = Division.create(value_a, value_b)
-        Calculator.add_calculation_to_history(division)
-        return Calculator.get_result_of_latest_calculation_added()
