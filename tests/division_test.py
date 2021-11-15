d7949a1de9154e32a507a75b78b2ec41bd910e4f
Hollinss <hj84@njit.edu>
2021-11-15 17:24:42 -0500

    TASK: Updated division_test.py

--- tests/division_test.py
+++ tests/division_test.py
@@ -3,16 +3,17 @@ import pytest
 
 from calc_mod.calculations.division import Division
 
+
 def test_calculation_division():
     """test division"""
-    mynumbers = (2.0,1.0)
+    mynumbers = (2.0, 1.0)
     division = Division(mynumbers)
     assert division.get_result() == 0.5
 
-#test for division by zero
-def test_divisio_by_zero():
+
+# test for division by zero
+def test_division_by_zero():
     """test division by zero"""
     mynumbers = (0.0, 2.0)
-    division = Division(mynumbers)
-    with pytest.raises(ZeroDivisionError):
-        assert division.get_result() == 0.0
+    Division(mynumbers)
+    assert pytest.raises(ZeroDivisionError)
