c5bce144fa6841edac7f929f38cb72bf2fd24ec0
Hollinss <hj84@njit.edu>
2021-11-19 23:57:10 -0500

    TASK: Updated Multiplication and Division tests with csv reading compatability

--- tests/division_test.py
+++ tests/division_test.py
@@ -3,6 +3,8 @@ import pytest
 
 from calc_mod.calculations.division import Division
 
+import pandas as pd
+
 
 def test_calculation_division():
     """test division"""
@@ -17,3 +19,14 @@ def test_division_by_zero():
     mynumbers = (0.0, 2.0)
     Division(mynumbers)
     assert pytest.raises(ZeroDivisionError)
+
+def test_calculation_division_csv():
+    """testing multiplication from imported csv"""
+    file = pd.read_csv('test_data/division.csv')
+    for index, row in file.iterrows():
+        #Arrange
+        mynumbers = (row['value_1'], row['value_2'])
+        #Act
+        division = Division(mynumbers)
+        # Assert
+        assert division.get_result() == row['result']
--- tests/multiplication_test.py
+++ tests/multiplication_test.py
@@ -1,9 +1,21 @@
 """Multiplication test"""
 from calc_mod.calculations.multiplication import Multiplication
 
+import pandas as pd
+
 def test_calculation_multiplication():
     """test multiplication"""
     mynumbers = (4.0,5.0,6.0)
     multiplication = Multiplication(mynumbers)
     assert multiplication.get_result() == 120.0
-    
+
+def test_calculation_multiplication_csv():
+    """testing multiplication from imported csv"""
+    file = pd.read_csv('test_data/multiplication.csv')
+    for index, row in file.iterrows():
+        #Arrange
+        mynumbers = (row['value_1'], row['value_2'])
+        #Act
+        multiplication = Multiplication(mynumbers)
+        # Assert
+        assert multiplication.get_result() == row['result']
