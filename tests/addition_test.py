45a4f7826f34c2f70fbb449e807842900d641fc5
Hollinss <hj84@njit.edu>
2021-11-28 16:31:18 -0500

    TASK: cleaned up addition_test.py

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -11,18 +11,7 @@ def test_calculation_addition():
     # Act
     addition = Addition(mynumbers)
     # Assert
-    assert addition.get_result() == result
-
-    # -s for pytest to print out
-    # read into dataframe
-    # once you get dataframe to print, you can print to console
-    # datamanager folder: reads and writes csv's
-
-    # data_frame = pandas.read_csv(filename)
-    # for row in data_frame
-    # for key,value in row
-    # result
-    # list/tuple of the values that are the input for calculation
+    assert addition.get_result() == 33.5
 
 def test_calculation_addition_csv():
     """testing addition"""
